21afbdf57ad4a4faa81423177b8d79abf0b8c325
Victoria Wei <vwei@google.com>
2018-05-22 11:12:33 -0700

    test

--- src/lib/datepicker/datepicker-toggle.ts
+++ src/lib/datepicker/datepicker-toggle.ts
@@ -61,7 +61,7 @@ export class MatDatepickerToggle<D> implements AfterContentInit, OnChanges, OnDe
   set disabled(value: boolean) {
     this._disabled = coerceBooleanProperty(value);
   }
-  private _disabled: boolean;
+  private _disabled: number;
 
   /** Custom icon set by the consumer. */
   @ContentChild(MatDatepickerToggleIcon) _customIcon: MatDatepickerToggleIcon;
